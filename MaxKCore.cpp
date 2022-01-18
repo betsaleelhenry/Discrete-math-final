7840849417bed8f8032f7ca5b767f4cb6efe4321
HenryBetsaleel <wilsonsomdb@gmail.com>
2022-01-18 23:01:02 +0800

    class

--- MaxKCore.cpp
+++ MaxKCore.cpp
@@ -6,14 +6,14 @@
 
 #include <stdbool.h>
 #include <iostream> 
-//#include <cmath>
-//#include <cstdlib> 
-//clock_t  t_end,t_start = clock(); 
+#include <cstdlib>
+
 using namespace std;
 
-
 int Cardinality = 0;
+class NetworkXforCpp{
 
+public:
 bool **Adj_Network()
 {
     bool **Network = (bool **)malloc(sizeof(bool *) * 1000);
@@ -33,6 +33,7 @@ bool **Adj_Network()
 return Network;
 }
 
+//this help us save memory by removing the obsolete data from memory.
 void freeNetwork(bool **Network)
 {
     for (int i = 0; i < 1000; i++)
@@ -41,7 +42,8 @@ void freeNetwork(bool **Network)
     }
     free(Network);
 }
-
+//This function gets the input from the automatic handler 
+//and pasted it to our two variables x and y that later on will be indexes of our adjacency matrix
 void GetData(bool **Network)
 {
     int x, y;
@@ -54,6 +56,7 @@ void GetData(bool **Network)
         Cardinality++;
 }
 
+//gets the values of eaches of the notches.
 int *getNotches(bool **Network)
 {
 int *Notches = (int *)malloc(sizeof(int) * Cardinality); 
@@ -74,6 +77,13 @@ for (int i = 0; i < Cardinality; i++)
 }
 return Notches;
 }
+
+
+
+
+
+
+//deletes the vertices until theres none for the particular core.
 
 bool **deleteVertex(bool **Network, int redundant)
 {
@@ -102,7 +112,7 @@ for (int i = 0; i < Cardinality; i++)
         Output[i][j] = Network[i][j];
     }
 }
-while (1)
+while (true)
 {
     int *Notches = getNotches(Output);
     int sum = 0;
@@ -131,7 +141,7 @@ while (1)
 }
 return Output;
 }
-
+//this function is actual a checkpoint to see if we erased the graph, then we would know that we have passed the maximum
 bool NullTab(bool **Network)
 {
     for (int i = 0; i < Cardinality; i++)
@@ -146,7 +156,7 @@ bool NullTab(bool **Network)
     }
     return true;
 }
-
+//This outputs our final result along with the first part.
 void Print_Max_KCore(bool **Network)
 {
     for (int i = 0; i < Cardinality; i++)
@@ -160,7 +170,7 @@ void Print_Max_KCore(bool **Network)
         }
     }
 }
-
+//pruning operation consists of gething read of the unnecessary parts.
 void Pruned(bool **Network)
 {
     for (int i = 0; i < Cardinality; i++)
@@ -170,30 +180,36 @@ void Pruned(bool **Network)
     free(Network);
 }
 
+
+};
+
+
 int main()
 {
-bool **Network = Adj_Network(); 
-GetData(Network);
+NetworkXforCpp Nx;
+bool **Network = Nx.Adj_Network(); 
+Nx.GetData(Network);
 bool **Output;
-
+//this for loop takes into consideration the k value until it reaches the maximum core, then it would break.
 for (int k = 1;; k++)
 {
-    Output = Prune_core(k, Network);
-    if (NullTab(Output))
+    Output = Nx.Prune_core(k, Network);
+    //the if statement guaranties that we have a maximum k core (k-1), which is also output in our final results first part.
+    if (Nx.NullTab(Output))
     {
-        Pruned(Output);
-        Output = Prune_core(k - 1, Network);
+       Nx.Pruned(Output);
+        Output = Nx.Prune_core(k - 1, Network);
         cout<<(k-1)<<"-core"<<endl; 
-        break;
+        break;//means we are done with the iterations to find the maximum core.
     }
     else
     {
-    Pruned(Output);
+    Nx.Pruned(Output);
     }
 }
-Print_Max_KCore(Output);
-freeNetwork(Network);
-Pruned(Output);
+Nx.Print_Max_KCore(Output);
+Nx.freeNetwork(Network);
+Nx.Pruned(Output);
 
 return 0;
 }
